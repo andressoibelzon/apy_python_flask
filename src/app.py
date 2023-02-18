d303f73c8423fafed37590298b1d1eced0c330ab
Andres Soibelzon <a.soibelzon@gmail.com>
2023-02-18 09:49:23 +0000

    ejercicio terminado

--- src/app.py
+++ src/app.py
@@ -10,16 +10,28 @@ todos = [
         } 
         ]
 
-# @app.route('/todos', methods=['GET'])
-# def hello_world():
-#     json_text = jsonify(todos)
-#     return json_text
+@app.route('/todos', methods=['GET'])
+def hello_world():
+    json_text = jsonify(todos)
+    return json_text
 
 @app.route('/todos', methods=['POST'])
 def add_new_todo():
     request_body = request.data
     print("Incoming request with the following body", request_body)
-    return 'Response for the POST todo'
+    todos.append(request_body)
+    return jsonify(todos)
+
+@app.route('/todos', methods=['DELETE'])
+def delete_todo(position):
+    todos.pop(position)
+    return jsonify(todos)
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    todos.pop(position)
+    print("This is the position to delete: ", position)
+    return jsonify(todos)
 
 
 
